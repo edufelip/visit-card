74d54a0c72030e2aec213909cdc5e3cd28318567
Eduardo Felipe <edu_felip@hotmail.com>
2021-07-31 12:48:27 -0300

    add recyclerview to list fragment

---
+++ app/src/main/java/com/example/visitcard/ui/adapters/ListAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.visitcard.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.visitcard.databinding.RecyclerBusinessCardBinding
+import com.example.visitcard.domain.models.Card
+import com.example.visitcard.ui.fragments.ListFragmentDirections
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+    private var cardList = listOf<Card>()
+
+    class MyViewHolder(private val itemBinding: RecyclerBusinessCardBinding): RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(card: Card) {
+            itemBinding.itemName.text = card.name
+            itemBinding.itemEmail.text = card.email
+            itemBinding.itemTelephone.text = card.tel
+            itemBinding.itemBusiness.text = card.business
+            itemBinding.cardContainer.setOnClickListener {
+                val action = ListFragmentDirections.actionListFragmentToEditFragment(card)
+                Navigation.findNavController(itemBinding.root).navigate(action)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = RecyclerBusinessCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val card: Card = cardList[position]
+        holder.bind(card)
+    }
+
+    override fun getItemCount(): Int {
+        return cardList.size
+    }
+
+    fun setCardList(cards: List<Card>) {
+        this.cardList = cards
+        notifyDataSetChanged()
+    }
+
+}
